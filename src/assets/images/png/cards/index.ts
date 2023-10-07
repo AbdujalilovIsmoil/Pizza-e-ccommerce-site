10cfd90592cf412c7c83faf49a34a58e543fa84b
AbdujalilovIsmoil <ismoilbek20050421@gmail.com>
2023-10-07 16:28:55 +0300

    feat: Added Group Card

---
+++ src/assets/images/png/cards/index.ts
@@ -0,0 +1,6 @@
+import Card1 from "./cards-1.png";
+import Card2 from "./cards-2.png";
+import Card3 from "./cards-3.png";
+import Card4 from "./cards-4.png";
+
+export { Card1, Card2, Card3, Card4 };
--- src/assets/images/png/index.ts
+++ src/assets/images/png/index.ts
@@ -1,3 +1,4 @@
 import { Cart1 } from "./cart";
+import { Card1, Card2, Card3, Card4 } from "./cards";
 
-export { Cart1 };
+export { Cart1, Card1, Card2, Card3, Card4 };
