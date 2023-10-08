bd496f3287c3f73251c71933caab7e8a0fdcc468
AbdujalilovIsmoil <ismoilbek20050421@gmail.com>
2023-10-08 22:29:50 +0300

    feat: Added Footer

--- src/components/layouts/footer/index.tsx
+++ src/components/layouts/footer/index.tsx
@@ -13,30 +13,35 @@ const index = () => {
         <div className="container">
           <ul className="footer__list">
             <li className="footer__item">
-              <img
-                alt="intro"
-                title="intro"
-                src={HeaderListIntro}
-                className="footer__item-intro"
-              />
+              <a href="#">
+                <div className="footer__intro">
+                  <img
+                    alt="intro"
+                    title="intro"
+                    src={HeaderListIntro}
+                    className="footer__item-intro"
+                  />
+                  <h4 className="footer__item-title">Куда пицца</h4>
+                </div>
+              </a>
             </li>
             <li className="footer__item">
               <h4 className="footer__item-heading">Куда пицца</h4>
               <ul className="footer-categories">
                 <li className="footer-categories__item">
-                  <h4 className="footer-categories__item-heading">
+                  <a href="#" className="footer-categories__item-heading">
                     О компании
-                  </h4>
+                  </a>
                 </li>
                 <li className="footer-categories__item">
-                  <h4 className="footer-categories__item-heading">
+                  <a href="#" className="footer-categories__item-heading">
                     Пользовательское соглашение
-                  </h4>
+                  </a>
                 </li>
                 <li className="footer-categories__item">
-                  <h4 className="footer-categories__item-heading">
+                  <a href="#" className="footer-categories__item-heading">
                     Условия гарантии
-                  </h4>
+                  </a>
                 </li>
               </ul>
             </li>
@@ -44,18 +49,24 @@ const index = () => {
               <h4 className="footer__item-heading">Помощь</h4>
               <ul className="footer-categories">
                 <li className="footer-categories__item">
-                  <h4 className="footer-categories__item-heading">Ресторан</h4>
+                  <a href="#" className="footer-categories__item-heading">
+                    Ресторан
+                  </a>
                 </li>
                 <li className="footer-categories__item">
-                  <h4 className="footer-categories__item-heading">Контакты</h4>
+                  <a href="#" className="footer-categories__item-heading">
+                    Контакты
+                  </a>
                 </li>
                 <li className="footer-categories__item">
-                  <h4 className="footer-categories__item-heading">Поддержка</h4>
+                  <a href="#" className="footer-categories__item-heading">
+                    Поддержка
+                  </a>
                 </li>
                 <li className="footer-categories__item">
-                  <h4 className="footer-categories__item-heading">
+                  <a href="#" className="footer-categories__item-heading">
                     Отследить заказ
-                  </h4>
+                  </a>
                 </li>
               </ul>
             </li>
@@ -64,57 +75,72 @@ const index = () => {
               <ul className="footer-medias">
                 <li className="footer-medias__item">
                   <a href="#" target="_blank">
-                    <img
-                      title="phone"
-                      src={FooterIcon1}
-                      alt="footer-icon1"
-                      className="footer-medias__item-icon"
-                    />
-                    <h4 className="fotoer-medias__item-heading">
-                      +7 (926) 223-10-11
-                    </h4>
+                    <div className="footer-medias__container">
+                      <img
+                        title="phone"
+                        src={FooterIcon1}
+                        alt="footer-icon1"
+                        className="footer-medias__item-icon"
+                      />
+                      <h4 className="footer-medias__item-heading">
+                        +7 (926) 223-10-11
+                      </h4>
+                    </div>
                   </a>
                 </li>
                 <li className="footer-medias__item">
                   <a href="#" target="_blank">
-                    <img
-                      title="location"
-                      src={FooterIcon2}
-                      alt="footer-icon2"
-                      className="footer-medias__item-icon"
-                    />
-                    <h4 className="fotoer-medias__item-heading">
-                      Москва, ул. Юных Ленинцев, д.99
-                    </h4>
+                    <div className="footer-medias__container">
+                      <img
+                        title="location"
+                        src={FooterIcon2}
+                        alt="footer-icon2"
+                        className="footer-medias__item-icon"
+                      />
+                      <h4 className="footer-medias__item-heading">
+                        Москва, ул. Юных Ленинцев, д.99
+                      </h4>
+                    </div>
                   </a>
                 </li>
                 <ul className="footer-medias__list">
                   <li className="footer-medias__item">
                     <a href="#" target="_blank">
-                      <img
-                        title="facebook"
-                        src={FooterIcon3}
-                        alt="footer-icon3"
-                        className="footer-medias__item-icon"
-                      />
-                      <h4 className="fotoer-medias__item-heading">Facebok</h4>
+                      <div className="footer-medias__container">
+                        <img
+                          title="facebook"
+                          src={FooterIcon3}
+                          alt="footer-icon3"
+                          className="footer-medias__item-icon"
+                        />
+                        <h4 className="footer-medias__item-heading">Facebok</h4>
+                      </div>
                     </a>
                   </li>
                   <li className="footer-medias__item">
                     <a href="#" target="_blank">
-                      <img
-                        title="facebook"
-                        src={FooterIcon4}
-                        alt="footer-icon4"
-                        className="footer-medias__item-icon"
-                      />
-                      <h4 className="fotoer-medias__item-heading">Instagram</h4>
+                      <div className="footer-medias__container">
+                        <img
+                          title="facebook"
+                          src={FooterIcon4}
+                          alt="footer-icon4"
+                          className="footer-medias__item-icon"
+                        />
+                        <h4 className="footer-medias__item-heading">
+                          Instagram
+                        </h4>
+                      </div>
                     </a>
                   </li>
                 </ul>
               </ul>
             </li>
           </ul>
+          <div className="footer__copy">
+            <h4 className="footer__copy-heading">
+              &copy; Copyright {new Date().getFullYear()} — Куда Пицца
+            </h4>
+          </div>
         </div>
       </footer>
     </>
