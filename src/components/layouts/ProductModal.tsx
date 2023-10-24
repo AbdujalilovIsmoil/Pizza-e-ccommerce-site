3de0dc17c3c7e86b05bebe3cb3ee36f063626581
AbdujalilovIsmoil <ismoilbek20050421@gmail.com>
2023-10-24 17:54:07 +0300

    feat: Added product modal responsive codes

--- src/components/layouts/ProductModal.tsx
+++ src/components/layouts/ProductModal.tsx
@@ -54,6 +54,7 @@ const ProductModal = () => {
           <div className="product-modal__background">
             {productModalData?.length > 0 &&
               productModalData?.map((el: any) => {
+                console.log(el);
                 return (
                   <ul className="product-modal__list" key={el._id}>
                     <li className="product-modal__item">
@@ -65,70 +66,82 @@ const ProductModal = () => {
                       />
                     </li>
                     <li className="product-modal__item">
-                      <div className="item-header">
-                        <div className="item-header__box">
-                          <img
-                            alt="product-icon"
-                            src={ProductModalIcon2}
-                            className="item-header__box-icon"
-                          />
-                          <h4 className="item-header__box-heading">
-                            Пепперони по-деревенски
-                          </h4>
+                      <div className="product-modal__wrapper">
+                        <div className="item-header">
+                          <div className="item-header__box">
+                            <img
+                              alt="product-icon"
+                              src={ProductModalIcon2}
+                              className="item-header__box-icon"
+                            />
+                            <h4 className="item-header__box-heading">
+                              Пепперони по-деревенски
+                            </h4>
+                          </div>
+                          <div className="item-header__box">
+                            <img
+                              alt="product-warning"
+                              src={ProductModalIcon3}
+                              className="item-header__box-warning"
+                            />
+                          </div>
                         </div>
-                        <div className="item-header__box">
-                          <img
-                            alt="product-warning"
-                            src={ProductModalIcon3}
-                            className="item-header__box-warning"
-                          />
+                        <div className="product-modal__content">
+                          <h4 className="product-modal__content-name">
+                            {el.name}
+                          </h4>
+                          <h4 className="product-modal__content-description">
+                            {el.description}
+                          </h4>
                         </div>
                       </div>
                       <div className="product-modal__boxes">
-                        <ul className="item-buttons">
-                          <li className="item-buttons__box">
-                            <Input
-                              type="radio"
-                              id="button1"
-                              name="button-radio"
-                              className="item-buttons__box-input"
-                            />
-                            <label
-                              htmlFor="button1"
-                              className="item-buttons__box-btn"
-                            >
-                              20 см
-                            </label>
-                          </li>
-                          <li className="item-buttons__box">
-                            <Input
-                              type="radio"
-                              id="button2"
-                              name="button-radio"
-                              className="item-buttons__box-input"
-                            />
-                            <label
-                              htmlFor="button2"
-                              className="item-buttons__box-btn"
-                            >
-                              28 см
-                            </label>
-                          </li>
-                          <li className="item-buttons__box">
-                            <Input
-                              type="radio"
-                              id="button3"
-                              name="button-radio"
-                              className="item-buttons__box-input"
-                            />
-                            <label
-                              htmlFor="button3"
-                              className="item-buttons__box-btn"
-                            >
-                              33 см
-                            </label>
-                          </li>
-                        </ul>
+                        {el.tags === "Пицца" && (
+                          <ul className="item-buttons">
+                            <li className="item-buttons__box">
+                              <Input
+                                type="radio"
+                                id="button1"
+                                name="button-radio"
+                                className="item-buttons__box-input"
+                              />
+                              <label
+                                htmlFor="button1"
+                                className="item-buttons__box-btn"
+                              >
+                                20 см
+                              </label>
+                            </li>
+                            <li className="item-buttons__box">
+                              <Input
+                                type="radio"
+                                id="button2"
+                                name="button-radio"
+                                className="item-buttons__box-input"
+                              />
+                              <label
+                                htmlFor="button2"
+                                className="item-buttons__box-btn"
+                              >
+                                28 см
+                              </label>
+                            </li>
+                            <li className="item-buttons__box">
+                              <Input
+                                type="radio"
+                                id="button3"
+                                name="button-radio"
+                                className="item-buttons__box-input"
+                              />
+                              <label
+                                htmlFor="button3"
+                                className="item-buttons__box-btn"
+                              >
+                                33 см
+                              </label>
+                            </li>
+                          </ul>
+                        )}
                         <ul className="product-modal-bottom">
                           <li className="product-modal-bottom__box">
                             <h4 className="product-modal-bottom__box-heading">
